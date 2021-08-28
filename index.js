4cc45b7e34e0e5ed1db9a0d5dadfdd1a09feb9d0
Antonio <atcarter005@gmail.com>
2021-08-28 12:46:32 -0500

    commit tree 342c10598babf881728ef2f261b294abb1e3b22f

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, type) {
+  let arr = []
+  for (let i = 0; i < array.length; i++) {
+    arr.push(`Thank you, ${array[i]}, for the wonderful ${type} gift!`)
+  }
+  return arr
+}
+
+function countDown(num) {
+  while (num > -1) {
+    console.log(num)
+    num--
+  }
+}
